9551314f05668cc31e329918109edb82a8b6a6ae
barisaksu <turkasoft@yandex.com>
2022-08-26 20:10:47 +0000

    Deploying to gh-pages from @ barisaksu/terminal@34bba66d2944cd0d73d6db1ea8c2851bf7923d97 🚀

---
+++ _next/static/zVKH41Ph2B90_ZcNMCHat/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-6cb4e9e651d3cf06.js"],"/404":["static/chunks/pages/404-ebe2d8128625c907.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/404","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
